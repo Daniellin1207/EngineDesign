af50c0c7e9ba09ba45ddbebd35dfb967f29b1ab5
Daniel.Lin <daniel@daniellindemacbook-pro.local>
2019-09-05 18:28:57 +0800

    Initial Commit

---
+++ RayTracing1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracing1
+//
+//  Created by Daniel.Lin on 2019/9/5.
+//  Copyright © 2019 Daniel.Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
